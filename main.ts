1731c7c2b2fdd07d7854f45ac2742eb335542d15
eimosqwe <131914311+eimosqwe@users.noreply.github.com>
2023-04-26 15:06:37 -0500

    primero

--- main.ts
+++ main.ts
@@ -2,7 +2,16 @@ input.onButtonPressed(Button.A, function () {
     N4 = 10
     N3 = 20
     resultado = N3 * N4
+    basic.showNumber(resultado)
 })
+input.onButtonPressed(Button.B, function () {
+    N6 = 16
+    N5 = 25
+    resultado = N5 * N6
+    basic.showNumber(resultado)
+})
+let N5 = 0
+let N6 = 0
 let N3 = 0
 let N4 = 0
 let resultado = 0
