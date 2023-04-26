000d4306346830e12473b9032ebd20a2883e7ac5
eimosqwe <131914311+eimosqwe@users.noreply.github.com>
2023-04-26 14:57:35 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    N4 = 10
+    N3 = 20
+    resultado = N3 * N4
+})
+let N3 = 0
+let N4 = 0
+let resultado = 0
+let N1 = 25
+let N2 = 5
+resultado = N1 + N2
+basic.showNumber(resultado)
